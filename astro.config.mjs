1e86182007e760642c94b9149120ea7231b0e61d
AndriwRC <randriw4@gmail.com>
2025-05-15 23:41:43 -0500

    set up deployment via netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,4 +13,5 @@ export default defineConfig({
   },
 
   integrations: [react()],
+  adapter: netlify(),
 });
